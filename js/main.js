746f7ceda404557985bf39fd64728eb2e7f8da69
Nick DeMarco <nicholas_demarco@brown.edu>
2017-01-25 01:23:47 -0500

    [ND] WIP Homepage format looking okay. End of first day

--- js/main.js
+++ js/main.js
@@ -133,6 +133,34 @@ var main = {
   }
 };
 
+// For Timeline
+jQuery(document).ready(function($){
+  var timelineBlocks = $('.cd-timeline-block'),
+    offset = 0.8;
+
+  //hide timeline blocks which are outside the viewport
+  hideBlocks(timelineBlocks, offset);
+
+  //on scolling, show/animate timeline blocks when enter the viewport
+  $(window).on('scroll', function(){
+    (!window.requestAnimationFrame) 
+      ? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
+      : window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+  });
+
+  function hideBlocks(blocks, offset) {
+    blocks.each(function(){
+      ( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+    });
+  }
+
+  function showBlocks(blocks, offset) {
+    blocks.each(function(){
+      ( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+    });
+  }
+});
+
 // 2fc73a3a967e97599c9763d05e564189
 
 document.addEventListener('DOMContentLoaded', main.init);
